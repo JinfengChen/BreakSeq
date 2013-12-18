b7d7951748b66fa511006cc7e7da1f0092fc5a96
JinfengChen <jinfeng7chen@gmail.com>
2013-12-17 16:49:37 -0800

    com

---
+++ bin/Iseq.py
@@ -0,0 +1,65 @@
+#!/opt/Python/2.7.3/bin/python
+import sys
+from collections import defaultdict
+from numpy import *
+import re
+import os
+import argparse
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+def usage():
+    test="name"
+    message='''
+python Iseq.py --gff
+Get Insertion sequence in SV gff file
+GFF:
+Chr1    SVpipe  Deletion        1045592 1045658 .       .       .       Size=67;Method=pindel;INDEL=Deletion;Seq=TCAATCATTGGTACTCTTAATGGAAATTTAACACAAGCTCTATCCTTACAGAAAATTTCTTGATGTA;
+
+FNA:
+>Chr1:1045592-1045658:SVpipe
+TCAATCATTGGTACTCTTAATGGAAATTTAACACAAGCTCTATCCTTACAGAAAATTTCTTGATGTA
+
+    '''
+    print message
+
+
+'''
+Chr1	SVpipe	Deletion	1045592	1045658	.	.	.	Size=67;Method=pindel;INDEL=Deletion;Seq=TCAATCATTGGTACTCTTAATGGAAATTTAACACAAGCTCTATCCTTACAGAAAATTTCTTGATGTA;
+'''
+def Iseq(gff, fna):
+    s = re.compile(r'Seq=(.*?);')
+    ofile = open (fna, 'w+')
+    with open (gff, 'r') as filehd:
+        for line in filehd:
+            line = line.rstrip()
+            if len(line) > 2:
+                unit = re.split(r'\t',line)
+                m = s.search(unit[8])
+                if m:
+                    print line
+                    seq   = m.groups(0)[0]
+                    seqid = '%s:%s-%s:%s' % (unit[0], unit[3], unit[4], unit[1])
+                    newrecord = SeqRecord(Seq(seq),id=seqid,description="")
+	    	    SeqIO.write(newrecord,ofile,"fasta")
+                
+    ofile.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--gff')
+    parser.add_argument('-v', dest='verbose', action='store_true')
+    args = parser.parse_args()
+    try:
+        len(args.gff) > 0
+    except:
+        usage()
+        sys.exit(2)
+
+    fna = os.path.splitext(args.gff)[0] + '.fna'
+    Iseq(args.gff, fna)
+
+if __name__ == '__main__':
+    main()
+
--- bin/work.sh
+++ bin/work.sh
@@ -1,4 +1,6 @@
 head -n 20 ../../SV_postprocess/SV_merge/bin/Deletion/HEG4.Deletion.final.gff > test.gff
+head -n 20 ../../SV_postprocess/SV_merge/bin/Deletion/HEG4.insertion.final.gff > test.insertion.gff
+python Iseq.py --gff test.insertion.gff
 
 echo "python module in local"
 export PYTHONPATH=$PYTHONPATH:/rhome/cjinfeng/software/tools/breakseq-1.3/lib;
@@ -24,5 +26,7 @@ cat /rhome/cjinfeng/BigData/00.RD/GenomeAlign/Lastz/output/MSU7vsOPU/prenet_net/
 python Net_ChrName.py --input chain_net/MSU7_OPU.all_net_level1/net.level
 
 echo "test run svState.py"
-python /rhome/cjinfeng/software/tools/breakseq-1.3/bin/svState/svState.py test.gff ./ 50 > log 2> log2
+python /rhome/cjinfeng/software/tools/breakseq-1.3/bin/svState/svState.py test.gff ./win200 200 > log 2> log2
+python /rhome/cjinfeng/software/tools/breakseq-1.3/bin/svState/svState.py test.gff ./win1000 > log 2> log2
+python /rhome/cjinfeng/software/tools/breakseq-1.3/bin/svState/svState.py test.insertion.gff ./win50_insertion/ 50 > log 2> log2
 
